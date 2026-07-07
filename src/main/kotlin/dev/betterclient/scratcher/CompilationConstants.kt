809a2bc902cec149c258b998d79b486564978ef5
betterclient <iamanaltlolwhypog@gmail.com>
2026-07-08 01:53:16 +0300

    other

--- src/main/kotlin/dev/betterclient/scratcher/CompilationConstants.kt
+++ src/main/kotlin/dev/betterclient/scratcher/CompilationConstants.kt
@@ -11,10 +11,10 @@ object CompilationConstants {
 
     //garbage collector settings
     const val MANUAL_MEMORY = false //disable garbage collector to use manual memory management
-    const val AUTOMATIC_GC = true //enable automatic gc::collect every 1 second (this will make it so you have to call collect)
+    const val AUTOMATIC_GC = true //enable automatic gc::collect every 1 second (this will make it so you have to call gc::collect yourself)
     const val REFLECT_GC = true //use hacked blocks to do top level marking in garbage collection
 
     //improve performance by disabling safety, only disable if you already tested your project with them enabled
-    const val DISABLE_INDEX_OUT_OF_BOUNDS = false //disable index out of bounds checking
-    const val DISABLE_TYPE_CHECKER = false //disables the runtime type checker
+    const val DISABLE_INDEX_OUT_OF_BOUNDS = true //disable index out of bounds checking
+    const val DISABLE_TYPE_CHECKER = true //disable the runtime type checker
 }
